4b4fabc03930c56bd496957ac0742fa315135251
Eric Simpson <52686452+ericssimpson@users.noreply.github.com>
2025-07-15 08:49:38 -0700

    try(vercel): getting rid of purgecss for right now :(

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,34 +1,7 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('@tailwindcss/postcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          purgecss({
-            content: [
-              './index.html',
-              './src/**/*.vue',
-              './src/**/*.ts',
-              './src/**/*.js',
-            ],
-            safelist: {
-              // Add any classes that are added dynamically and might be missed by the scanner
-              // For example, from PrimeVue or other libraries
-              standard: [
-                /p-\w+/, // PrimeVue components
-                /pi-\w+/, // PrimeIcons
-                /mapboxgl-\w+/, // Mapbox GL JS
-              ],
-              deep: [],
-              greedy: [],
-              keyframes: [],
-              variables: [],
-            },
-          }),
-        ]
-      : []),
-  ],
+  plugins: {
+    'postcss-import': {},
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
 }
